ca83c72453f1a37ffdfb167cdc80628a12f9f155
Programmer2011bird <maysamshaker7@gmail.com>
2025-08-12 18:08:45 +0330

    Cleaning up the code / moving the possibly repetative code to __init__

--- API.py
+++ API.py
@@ -8,7 +8,6 @@ class scraper:
         self.ARTIST_NAME: str = artist_name.lower().replace(" ", "-")
         self.BASE_URL: str = f"https://genius.com/{self.ARTIST_NAME}-{self.SONG_NAME}-lyrics"
 
-    def get_lyrics(self):
         self.headers = {
             "Accept": "text/html",
             "Accept-Encoding": "gzip, deflate, br, zstd",   
@@ -18,10 +17,9 @@ class scraper:
         }
 
         self.response = requests.get(self.BASE_URL, headers=self.headers)
-        with open("test.html", "w+") as file:
-            file.write(str(self.response))
         self.soup = BeautifulSoup(self.response.text, "html.parser")
 
+    def get_lyrics(self) -> str:
         lyricsPreview = self.soup.find("div", attrs={"class":"LyricsHeader__Container-sc-5e4b7146-1 hFsUgC"})
         lyricsPreview = str(lyricsPreview.text)
         
@@ -35,7 +33,7 @@ class scraper:
         lyrics = str(lyrics.text)
         lyrics = lyrics.replace(lyricsPreview, "")
 
-        print(lyrics)
+        return lyrics
 
 
 if __name__ == "__main__":
