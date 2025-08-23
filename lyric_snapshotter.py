640ae5ccd8dfdf964aa1cc95b04757ec67c0c114
Programmer2011bird <maysamshaker7@gmail.com>
2025-08-23 17:21:44 +0330

    To-dos

---
+++ lyric_snapshotter.py
@@ -0,0 +1,14 @@
+def snapshot_lyrics(lyrics: str):
+    # 1: Snapshot the lyrics : From the start, just two or more sentences
+    pass
+
+def add_to_html(content: str, html_file_name: str):
+    # 2: Add the snapshotted lyrics to an html placeholder :
+    # - just like spotify, but a nice, dark blue color
+    # - On top, a big text showing the name of the song and another smaller one under it with the singer's name
+    # - Fonts : Spotify Circular
+    pass
+
+def turn_html_to_image(html_file_name: str):
+    # 3: turn the html to an image with imgkit
+    pass
